006d38f958de61b7959b958bba2b252e315fa128
twdenz <t.janwijgers@gmail.com>
2020-12-28 21:10:16 +0100

    Updated risk.py

--- risk.py
+++ risk.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
-print("Input Values")
+# risk = float(input("\tRisk percentage:\t"))
+risk = 1
+
+print("\nInput Values\n")
 tot_acc = float(input("\tTotal account:\t"))
 enter = float(input("\tEntry price:\t"))
 sl = float(input("\tStop loss:\t"))
-# risk = float(input("\tRisk percentage:\t"))
+print(f"\tAcc. Risk:\t{risk}%")
 
-risk = 1
 
 def calculate(tot_acc, enter, sl, risk):
 	if enter > sl:
@@ -18,12 +20,13 @@ def calculate(tot_acc, enter, sl, risk):
 	risk_abs = (risk/100)*tot_acc
 	pos = risk_abs / sl_diff_p
 	max_loss =  pos * sl_diff_p
+	print()
 	print("________________________________\n")
-	print("Trade Values")
-	print(f"\tMax Position:\t{pos}")
-	print(f"\tRisk Perc.:\t{round(sl_diff_p*100, 2)}")
-	print(f"\tMax Loss\t{max_loss}")
+	print("Trade\n")
 	print(f"\tTrade Type:\t{trade_type}")
+	print(f"\tMax Position:\t{round(pos)}")
+	print(f"\tRisk Perc:\t{round(sl_diff_p*100, 2)}%")
+	print(f"\tMax Loss\t{max_loss}\n")
 
 
 if __name__ == "__main__":
